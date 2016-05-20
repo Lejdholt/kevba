d23eeec87cbabf57003b156c7b2bb9b621d70a6e
Gustaf-Soderqvist <gustaf@infozone.se>
2016-05-20 22:49:50 +0200

    Some intial Controllers and epic Service

---
+++ kevba/Web/Controllers/API/MovieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Web.API
+{
+    [Route("api/[controller]")]
+    public class MovieController : Controller
+    {
+
+        static string _address = "http://api.themoviedb.org/3/";
+        private string result;
+
+        // GET api/values
+        public async Task<IEnumerable<string>> Get()
+        {
+            var result = await GetExternalResponse();
+
+            return new string[] { result, "value2" };
+        }
+
+        private async Task<string> GetExternalResponse()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(_address);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
+}

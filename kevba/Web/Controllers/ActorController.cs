fced76b8a3a7e01dca67dec29c46ee8c0844039c
Gustaf-Soderqvist <gustaf@infozone.se>
2016-05-21 19:50:23 +0200

    New controller 'ActorController'

---
+++ kevba/Web/Controllers/ActorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Data;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ActorController : ApiController
+    {
+        [HttpGet]
+        public ActorConnection GetConnection(string from, string to)
+        {
+            var service = new MovieService();
+
+            return service.GetConnection(from, to);
+        }
+    }
+}

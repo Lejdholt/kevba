5dddd28a9e962707458779b02bf227dee6ab7602
Jesper Augustsson <jesper@auddly.com>
2016-05-21 19:23:36 +0200

    front-end setup

---
+++ kevba/Web/app/movie/movie-controller.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('movieController');
+
+    function movie_controller() {
+        var vm = this;
+        
+
+    }
+})();
---
+++ kevba/Web/app/routes.js
@@ -0,0 +1,21 @@
+﻿(function () {
+
+    angular.module('app')
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state('home', {
+                    url: "/",
+                    views: {
+                        'main': {
+                            templateUrl: "partials/movie.html",
+                            controller: "movieController",
+                            controllerAs: 'vm'
+                        }
+
+                    }
+                })
+        });
+})();
